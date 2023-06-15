f2597e34521096639f96fc6a9144c657c447b314
Kkoli1 <kkolikkolikkoli@gmail.com>
2023-06-15 23:01:12 +0800

    removed gsap: test

--- src/components/LeftPillar.jsx
+++ src/components/LeftPillar.jsx
@@ -1,16 +1,9 @@
 
 import '../App.css'
-import gsap from 'gsap';
-import {useRef, useEffect} from 'react';
 
 export default function LeftPillar() {
-    const leftRef = useRef(null);
-    useEffect(() => {
-        const el = leftRef.current;
-        gsap.fromTo(el, {xPercent: -500}, {xPercent: 0, duration: 1.2})
-    }, [])
     return (
-        <div className="pillar" id='left-pillar' ref={leftRef}>
+        <div className="pillar" id='left-pillar' >
             <div className="linkedin-img">
                 <img src="src\assets\linkedin.png" alt="" srcset="" />
             </div>
