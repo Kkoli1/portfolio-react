05ca9cd1ad0c7b0c065f69631e0665395b7b49ec
Kkoli1 <kkolikkolikkoli@gmail.com>
2023-06-15 23:11:24 +0800

    test

--- src/components/LeftPillar.jsx
+++ src/components/LeftPillar.jsx
@@ -1,14 +1,21 @@
 
 import '../App.css'
+import gsap from 'gsap';
+import {useRef, useEffect} from 'react';
 
 export default function LeftPillar() {
+    const leftRef = useRef(null);
+    useEffect(() => {
+        const el = leftRef.current;
+        gsap.fromTo(el, {xPercent: -500}, {xPercent: 0, duration: 1.2})
+    }, [])
     return (
-        <div className="pillar" id='left-pillar' >
+        <div className="pillar" id='left-pillar' ref={leftRef}>
             <div className="linkedin-img">
-                <img src="src/assets/linkedin.png" alt="" srcset="" />
+                <img src="src\assets\linkedin.png" alt="" srcset="" />
             </div>
             <div className="github-img">
-                <img src="src/assets/github.png" alt="" srcset="" />
+                <img src="src\assets\github.png" alt="" srcset="" />
             </div>
             <div className="vertical-line"></div>
         </div>
