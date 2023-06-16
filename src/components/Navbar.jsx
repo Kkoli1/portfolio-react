c70ef2f58477feb20cf255743c6e816f3fa159c4
Akio Allen <kkolikkolikkoli@gmail.com>
2023-06-16 15:20:10 +0800

    added new class name for nav items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 
 import '../App.css';
 import gsap from 'gsap';
-import {useRef, useEffect} from 'react';
+import { useRef, useEffect } from 'react';
 
 export default function Navbar() {
     const navRef = useRef(null);
@@ -10,7 +10,7 @@ export default function Navbar() {
         gsap.fromTo(el, {yPercent: -100}, {yPercent: 0, duration: 1})
     }, [])
     return (
-        
+
         <>
             <div className="navbar-wrapper" ref={navRef}>
                 <div className="row-w-padding">
@@ -18,10 +18,10 @@ export default function Navbar() {
                         <h1>kio</h1>
                     </a>
                     <div className="links">
-                        <a href="" className="link" id="about"><h3>abt</h3></a>
-                        <a href="" className="link" id="experience"><h3>exp</h3></a>
-                        <a href="" className="link" id="projects"><h3>proj</h3></a>
-                        <div className="toggle-theme"><h3>dark</h3></div>
+                        <a href="" className="nav-item link" id="about"><h3>abt</h3></a>
+                        <a href="" className="nav-item link" id="experience"><h3>exp</h3></a>
+                        <a href="" className="nav-item link" id="projects"><h3>proj</h3></a>
+                        <div className="nav-item toggle-theme"><h3>dark</h3></div>
                     </div>
                     <div class="hamburger-menu">
                         <div class="hamburger-bar"></div>
