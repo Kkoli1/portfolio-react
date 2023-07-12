8437f89b8340558d323216678ee8a21570b52b1c
Kkoli1 <kkolikkolikkoli@gmail.com>
2023-07-12 15:38:57 +0800

    added animation for about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -14,37 +14,78 @@ export default function About() {
     // }, [])
 
     useEffect(() => {
-        const timeline1 = gsap.timeline();
+        const timeline1 = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".about-section-pin",
+                pin: true,
+                start: "center center",
+                end: "bottom center",
+                scrub: 1,
+                markers: true,
+            },
+        });
 
-        timeline1.from(".about-text[data-text='random']", {
-            
-            color: "red",
+         timeline1.to(".about-text[data-text='random']", {
+            fontSize: "2em",
+            x: "-50%",
+            y: "130%",
             duration: 3,
         })
-        const animation1 = gsap.fromTo(".test-text-1", { x: 1100 }, {
-            x: 0, y: 400,duration: 3, scrollTrigger: {
-                trigger: ".test-text-1",
-                start: "top",
-                end: "+=500 top",
-                scrub: 1,
-                pin: true,
-            }
-        })
 
-        const animation2 = gsap.fromTo(".test-text-2", { x: -1100 }, {
-            x: 0, y: 300,duration: 3, scrollTrigger: {
-                trigger: ".test-text-2",
-                start: "top",
-                end: "bottom +=1000",
-                pin: true,
-                scrub: 1,
-            }
-        })
+        timeline1.to(".about-text[data-text='words']", {
+            fontSize: "2em",
+            x: "50%",
+            y: "100%",
+            duration: 3,
+        },0)
+
+        timeline1.to(".about-text[data-text='mind']", {
+            fontSize: "2em",
+            x: "-50%",
+            y: "80%",
+            duration: 3,
+        },0)
+
+        timeline1.to(".about-text[data-text='mind']", {
+            fontSize: "2em",
+            x: "-50%",
+            y: "80%",
+            duration: 3,
+        },0)
+
+        timeline1.to(".about-img", {
+            y: "-60%",
+            width: 180,
+            height: 250,
+            backgroundSize: "150%",
+            duration: 3,
+        },0)
+
+        // const animation1 = gsap.fromTo(".test-text-1", { x: 1100 }, {
+        //     x: 0, y: 400, duration: 3, scrollTrigger: {
+        //         trigger: ".test-text-1",
+        //         start: "top",
+        //         end: "+=500 top",
+        //         scrub: 1,
+        //         pin: true,
+        //     }
+        // })
+
+        // const animation2 = gsap.fromTo(".test-text-2", { x: -1100 }, {
+        //     x: 0, y: 300, duration: 3, scrollTrigger: {
+        //         trigger: ".test-text-2",
+        //         start: "top",
+        //         end: "bottom +=1000",
+        //         pin: true,
+        //         scrub: 1,
+        //     }
+        // })
 
         return () => {
             timeline1.kill();
-            animation1.kill();
-            animation2.kill();
+            // randomAnimation.kill();
+            // wordsAnimation.kill();
+            // inMindAnimation.kill();
         };
 
     }, []);
@@ -54,7 +95,7 @@ export default function About() {
                 <div className="about-section-pin">
                     <div className="about-text" data-text="random">RANDOM</div>
                     <div className="about-text" data-text="words">WORDS</div>
-                    <div className="about-text" data-text="in mind">IN MIND</div>
+                    <div className="about-text" data-text="mind">IN MIND</div>
                     <div className="about-img"></div>
                 </div>
             </div>
