6450cd223047137cb469c713809b272a4f654281
Akio Yapan <akio.yapan@orangeandbronze.com>
2024-02-11 13:29:27 +0800

    applied prettify

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
-import '../App.css'
-import gsap from 'gsap';
-import { useEffect } from 'react';
+import "../App.css";
+import gsap from "gsap";
+import { useEffect } from "react";
 
 export default function Hero() {
   useEffect(() => {
@@ -66,25 +66,21 @@ export default function Hero() {
         y: 0,
         duration: 2.5,
         opacity: 1,
-        ease: 'power2.inOut',
+        ease: "power2.inOut",
         stagger: 0.8,
-
       }
     );
 
-    timeline.to(
-      ".line",
-      {
-        width: "100%",
-        duration: 2.5,
-        ease: 'power2.inOut',
-      },
-    );
+    timeline.to(".line", {
+      width: "100%",
+      duration: 2.5,
+      ease: "power2.inOut",
+    });
 
     timeline.fromTo(
       ".desc",
       {
-        // opacity: 0, 
+        // opacity: 0,
         zIndex: 0,
         y: "-110%",
       },
@@ -92,29 +88,50 @@ export default function Hero() {
         y: 0,
         // opacity: 1,
         duration: 2.5,
-        ease: 'power2.inOut',
-      },
+        ease: "power2.inOut",
+      }
     );
 
     // Return a function to kill the timeline
     return () => {
       timeline.kill();
     };
-
-
-
-
-  }, [])
+  }, []);
   return (
     <>
       <div id="hero-wrapper">
         <div className="hero-main-content">
           {/* <div className="main"><h2>hi, i'm <span>akio allen</span></h2></div> */}
-          <div className="test"><h2><div className="main-letter-container"><div className="main-letter">h</div><div className="main-letter">i</div><div className="main-letter">,</div><div className="letter-spacer"></div><div className="main-letter">i</div><div className="main-letter">'</div><div className="main-letter">m</div><div className="letter-spacer"></div><div className="main-letter name">a</div><div className="main-letter name">k</div><div className="main-letter name">i</div><div className="main-letter name">o</div><div className="letter-spacer"></div><div className="main-letter name">a</div><div className="main-letter name">l</div><div className="main-letter name">l</div><div className="main-letter name">e</div><div className="main-letter name">n</div></div></h2></div>
+          <div className="test">
+            <h2>
+              <div className="main-letter-container">
+                <div className="main-letter">h</div>
+                <div className="main-letter">i</div>
+                <div className="main-letter">,</div>
+                <div className="letter-spacer"></div>
+                <div className="main-letter">i</div>
+                <div className="main-letter">&apos;</div>
+                <div className="main-letter">m</div>
+                <div className="letter-spacer"></div>
+                <div className="main-letter name">a</div>
+                <div className="main-letter name">k</div>
+                <div className="main-letter name">i</div>
+                <div className="main-letter name">o</div>
+                <div className="letter-spacer"></div>
+                <div className="main-letter name">a</div>
+                <div className="main-letter name">l</div>
+                <div className="main-letter name">l</div>
+                <div className="main-letter name">e</div>
+                <div className="main-letter name">n</div>
+              </div>
+            </h2>
+          </div>
           <div className="line"></div>
-          <div className="desc"><h2>a software engineer</h2></div>
+          <div className="desc">
+            <h2>a software engineer</h2>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
